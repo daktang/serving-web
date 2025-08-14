26293e4c8050b362896e214f2ecad77caffe5d44
daktang <90pizza@naver.com>
2025-08-14 15:58:07 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,46 +1,31 @@
-accept
-application/json, text/plain, */*
-accept-encoding
-gzip, deflate, br, zstd
-accept-language
-ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7
-connection
-keep-alive
-content-length
-2
-content-type
-application/json
-cookie
-authservice_session=MTc1NTE0MDg0N3xOd3dBTkV0RVVFOU9NMFpZUzBWSlZ6UllSbFZIUmpRM1RWcFFWRFZJU1VaWU16TktUalV6UjA5RVRFVTJOMVJNU1ZwQldVeE5WMEU9fEg2o_8EkhxJGlyA7PSPeuM0-ULQ6TAylgPCHZzwViqs
-host
-localhost:3000
-kubeflow-userid
-null
-namespace
-undefined
-origin
-https://localhost:3000
-project-id
-undefined
-referer
-https://localhost:3000/login
-sec-ch-ua
-"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"
-sec-ch-ua-mobile
-?0
-sec-ch-ua-platform
-"Windows"
-sec-fetch-dest
-empty
-sec-fetch-mode
-cors
-sec-fetch-site
-same-origin
-user-agent
-Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36
-x-mlops-action-id
-59
-x-mlops-menuid
-26
-x-mlops-namespace
-x-mlops-target
+window.config = {
+    namespace: 'aiserving-system',
+    rootContext: "",
+
+    apiGateway: {
+        base_url_V2: '//portal.aiserving.dev.aip.domain.net/api/v2/',
+        
+        dit_ext_base_url_V1: '//portal.aiserving.dev.aip.domain.net/ext-dit/api/v1/',
+        modelWebBackendUrl: '//portal.aiserving.dev.aip.domain.net/models',
+    },
+    coreApiUrl: '//portal.aiserving.dev.aip.domain.net/api/',
+    kubeflowLogout: 'https://auth.dev.aip.domain.net/auth/realms/aiplatform/protocol/openid-connect/logout?client_id=serve&post_logout_redirect_uri=https://portal.aiserving.dev.aip.domain.net/after-logout/logout',
+    sessionCheckInterval: 60,
+    deploymentName: ["ASP-DIT",],
+    
+    disableActionHistory: 'true',
+    
+    defaultAffinityConfig: 'servingPool',
+    
+    kubeflowURL: '//portal.aiserving.dev.aip.domain.net/',
+    kubeflowBaseUrl: 'portal.aiserving.dev.aip.domain.net',
+
+    servingExtBaseUrl: '/serving',
+    
+    metadata: { name: 'FrontendConfigmap' },
+    applicationName: "AI Serving Portal",
+    applicationSubName: "",
+    applicationRelease: "Release 1.7.0 | December 2023",
+buildId: '', commitHash: '', 
+    applicationTitle: "AI Serving Portal"
+};
