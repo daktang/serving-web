082d2468ae2c3afc2ae8112bc69c24990b7fef30
daktang <90pizza@naver.com>
2025-08-18 17:55:18 +0900

    Update local_test.js

--- test/local_test.js
+++ test/local_test.js
@@ -1,1 +1,73 @@
-#
+# Local Test
+1. General
+Request URL
+http://localhost:3000/coreproxy/v3/users/apikeys?page_size=2147483647
+Request Method
+GET
+Status Code
+200 OK
+Remote Address
+127.0.0.1:3000
+Referrer Policy
+strict-origin-when-cross-origin
+
+2. Request Headers
+accept
+application/json, text/plain, */*
+accept-encoding
+gzip, deflate, br, zstd
+accept-language
+ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7
+connection
+keep-alive
+cookie
+authservice_session=MTc1NTQ5NjQyMnxOd3dBTkRKRVFsZENTazlQV2xGWlVFUklWRWRIVTBWTFZ6SlBTakpGUjB4R1JFdE5OMVJNVGpaUE1saExVRTlTTWxCWFUwSlFVMEU9fDXm8wbBrYOrJ8FElezlHCoYTVuS6nRrpHkr6_8l6d2R
+host
+localhost:3000
+kubeflow-useriduser.id
+namespace
+ais-smoke-testing
+project-id
+85
+referer
+http://localhost:3000/api-key-mgmt
+sec-ch-ua
+"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"
+sec-ch-ua-mobile
+?0
+sec-ch-ua-platform
+"Windows"
+sec-fetch-dest
+empty
+sec-fetch-mode
+cors
+sec-fetch-site
+same-origin
+user-agent
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36
+
+
+3. Response Headers
+access-control-allow-origin
+*
+connection
+close
+content-length
+499
+content-type
+application/json
+date
+Mon, 18 Aug 2025 08:02:02 GMT
+server
+istio-envoy
+set-cookie
+authservice_session=MTc1NTQ5NjQyMnxOd3dBTkRKRVFsZENTazlQV2xGWlVFUklWRWRIVTBWTFZ6SlBTakpGUjB4R1JFdE5OMVJNVGpaUE1saExVRTlTTWxCWFUwSlFVMEU9fDXm8wbBrYOrJ8FElezlHCoYTVuS6nRrpHkr6_8l6d2R; Domain=.aiserving.dev.aip.domain.net; Expires=Tue, 19 Aug 2025 08:02:02 GMT; HttpOnly; Path=/
+vary
+Accept-Encoding
+x-envoy-upstream-service-time
+246
+x-powered-by
+Express
+
+
+# 
