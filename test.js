dfabf7d69b1dd6d8200cbfcfb2b8fcc1a055b7f8
daktang <90pizza@naver.com>
2025-08-18 14:26:30 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,51 @@
-https://portal.aiserving.dev.aip.domain.net/api/v2/users?id=2
-http://localhost:3000/coreproxy/v2/users?id=
+# 로컬 개발 환경
+
+accept
+application/json, text/plain, */*
+kubeflow-userid
+user.id
+namespace
+test-namespace
+project-id
+85
+referer
+http://localhost:3000/
+user-agent
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36
+
+
+# A 개발 환경
+accept
+application/json, text/plain, */*
+accept-encoding
+gzip, deflate, br, zstd
+accept-language
+ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7
+connection
+keep-alive
+cookie
+authservice_session=MTc1NTQ4OTM4N3xOd3dBTkZkTFJraENSMGROVXpKU1ZVZElSamMzTjBOTk4wNDNXVWxUU1ZORE4xZ3lRa1EzVlVOSldFTklUVE5OVTBOSlJEUkdVa0U9fJw3jfB2fiRj-oi3ox07rKpBuu-G-B0DJtxTH3OzEBxR
+host
+portal.aiserving.dev.aip.domain.net
+kubeflow-userid
+user.id
+namespace
+test-namespace
+project-id
+85
+referer
+https://portal.aiserving.dev.aip.domain.net/serving/model-server?ns=test-namespace&uId=2&projId=85&isAdmin=&projName=smoke-testing&accName=user.id&roleId=2&isApiCallAllowed=true
+sec-ch-ua
+"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"
+sec-ch-ua-mobile
+?0
+sec-ch-ua-platform
+"Windows"
+sec-fetch-dest
+empty
+sec-fetch-mode
+cors
+sec-fetch-site
+same-origin
+user-agent
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36
