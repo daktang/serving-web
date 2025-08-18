5fd3fac7b1569abe602ea5ed5c0153860b9f7677
daktang <90pizza@naver.com>
2025-08-18 12:26:50 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,52 @@
---- authenticate
-
 Request URL
-http://portal.aiserving.dev.aip.domain.net/api/v3/authenticate
+http://localhost:3000/coreproxy/v2/coreproxy/v3/authenticate
+Request Method
+POST
+Status Code
+500 Internal Server Error
+Remote Address
+127.0.0.1:3000
 Referrer Policy
 strict-origin-when-cross-origin
 
-request header
 accept
 application/json, text/plain, */*
+accept-encoding
+gzip, deflate, br, zstd
+accept-language
+ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7
+connection
+keep-alive
+content-length
+2
 content-type
 application/json
+cookie
+authservice_session=MTc1NTQ4NzQwN3xOd3dBTkZKQ05GQldNMFpaVDFsTFRWZEpTVVpTUmpWRVRWVlVSRnBJUWtoWU5FZFhXRmcwUjFOQ01rWkdSRXhHTWxOU1NrTktORUU9fFn0qtQx1nKbAsVHXKK2ycdkqWeDsiBvtbHaApFMwbyZ; oidc_state_csrf=MTc1NTQ4NzMxNXxOd3dBTkVvME1sRlBNMHhRVUVNMFdEYzBXRVJNUWxVMldrbEVRMFF5VTBSYU5GRlhUMUpQU0RJME5sVTNVMEpCVFZGWVNsRlJRVkU9fKw0-6lwS2nQsbfYSb-0faNbxJ1kTXz5jL3sZ3QVSRzX
+host
+localhost:3000
 kubeflow-userid
 null
 namespace
 undefined
+origin
+http://localhost:3000
 project-id
 undefined
 referer
-http://localhost:3000/
+http://localhost:3000/login
+sec-ch-ua
+"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"
+sec-ch-ua-mobile
+?0
+sec-ch-ua-platform
+"Windows"
+sec-fetch-dest
+empty
+sec-fetch-mode
+cors
+sec-fetch-site
+same-origin
 user-agent
 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36
 x-mlops-action-id
@@ -28,23 +57,81 @@ x-mlops-namespace
 x-mlops-target
 
 
---- logout
+잘되는 곳 예시
 Request URL
-https://auth.dev.aip.domain.net/auth/realms/aiplatform/protocol/openid-connect/logout?client_id=serve&post_logout_redirect_uri=http://localhost:3000/after-logout/logout?service=http://localhost/after-logout/logout
+https://portal.aiserving.dev.aip.domain.net/api/v3/authenticate
+Request Method
+POST
+Status Code
+200 OK
+Remote Address
+10.166.247.201:443
 Referrer Policy
 strict-origin-when-cross-origin
 
+응답
+access-control-allow-origin
+https://portal.aiserving.dev.aip.domain.net
+content-length
+375
+content-type
+application/json
+date
+Mon, 18 Aug 2025 03:23:29 GMT
+server
+istio-envoy
+set-cookie
+authservice_session=MTc1NTQ4NzQwN3xOd3dBTkZKQ05GQldNMFpaVDFsTFRWZEpTVVpTUmpWRVRWVlVSRnBJUWtoWU5FZFhXRmcwUjFOQ01rWkdSRXhHTWxOU1NrTktORUU9fFn0qtQx1nKbAsVHXKK2ycdkqWeDsiBvtbHaApFMwbyZ; Domain=.aiserving.dev.aip.domain.net; Expires=Tue, 19 Aug 2025 03:23:29 GMT; HttpOnly; Path=/
+vary
+Origin
+x-envoy-upstream-service-time
+245
 
-request header
+요청
+
+accept
+application/json, text/plain, */*
+accept-encoding
+gzip, deflate, br, zstd
+accept-language
+ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7
+connection
+keep-alive
+content-length
+2
+content-type
+application/json
+cookie
+authservice_session=MTc1NTQ4NzQwN3xOd3dBTkZKQ05GQldNMFpaVDFsTFRWZEpTVVpTUmpWRVRWVlVSRnBJUWtoWU5FZFhXRmcwUjFOQ01rWkdSRXhHTWxOU1NrTktORUU9fFn0qtQx1nKbAsVHXKK2ycdkqWeDsiBvtbHaApFMwbyZ
+host
+portal.aiserving.dev.aip.domain.net
+kubeflow-userid
+null
+namespace
+undefined
+origin
+https://portal.aiserving.dev.aip.domain.net
+project-id
+undefined
 referer
-http://localhost:3000/
+https://portal.aiserving.dev.aip.domain.net/login
 sec-ch-ua
 "Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"
 sec-ch-ua-mobile
 ?0
 sec-ch-ua-platform
 "Windows"
-upgrade-insecure-requests
-1
+sec-fetch-dest
+empty
+sec-fetch-mode
+cors
+sec-fetch-site
+same-origin
 user-agent
 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36
+x-mlops-action-id
+59
+x-mlops-menuid
+26
+x-mlops-namespace
+x-mlops-target
