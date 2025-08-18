0fc4c23f4b2aaf10f2f8b1ac4e6ad3ccf25d93f0
daktang <90pizza@naver.com>
2025-08-18 12:31:32 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
+# 로컬 authenticate 정리해줄게
+
+1. 제네럴
 Request URL
 http://localhost:3000/coreproxy/v2/coreproxy/v3/authenticate
 Request Method
@@ -9,6 +12,29 @@ Remote Address
 Referrer Policy
 strict-origin-when-cross-origin
 
+2. 응답 헤더
+access-control-allow-origin
+http://localhost:3000
+connection
+close
+content-length
+116
+content-type
+text/html; charset=utf-8
+date
+Mon, 18 Aug 2025 03:23:58 GMT
+server
+istio-envoy
+set-cookie
+authservice_session=MTc1NTQ4NzQwN3xOd3dBTkZKQ05GQldNMFpaVDFsTFRWZEpTVVpTUmpWRVRWVlVSRnBJUWtoWU5FZFhXRmcwUjFOQ01rWkdSRXhHTWxOU1NrTktORUU9fFn0qtQx1nKbAsVHXKK2ycdkqWeDsiBvtbHaApFMwbyZ; Domain=.aiserving.dev.aip.domain.net; Expires=Tue, 19 Aug 2025 03:23:58 GMT; HttpOnly; Path=/
+vary
+Origin, Accept-Encoding
+x-envoy-upstream-service-time
+3
+x-powered-by
+Express
+
+3. 요청 헤더
 accept
 application/json, text/plain, */*
 accept-encoding
@@ -56,8 +82,8 @@ x-mlops-menuid
 x-mlops-namespace
 x-mlops-target
 
-
-잘되는 곳 예시
+# 잘 되는 곳(A의 개발 환경 프론트엔드)의 정보
+1. 제네럴
 Request URL
 https://portal.aiserving.dev.aip.domain.net/api/v3/authenticate
 Request Method
@@ -65,11 +91,11 @@ POST
 Status Code
 200 OK
 Remote Address
-10.166.247.201:443
+# ip private 감춤 #
 Referrer Policy
 strict-origin-when-cross-origin
 
-응답
+2. 응답 헤더
 access-control-allow-origin
 https://portal.aiserving.dev.aip.domain.net
 content-length
@@ -87,8 +113,7 @@ Origin
 x-envoy-upstream-service-time
 245
 
-요청
-
+3. 요청 헤더
 accept
 application/json, text/plain, */*
 accept-encoding
@@ -135,3 +160,4 @@ x-mlops-menuid
 26
 x-mlops-namespace
 x-mlops-target
+
