c27035c36661da445453c4acded62dfba7dcb31e
daktang <90pizza@naver.com>
2025-08-18 15:43:50 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,19 @@
 # 로컬 개발 환경
+1. 제네럴
+Request URL
+http://portal.aiserving.dev.aip.domain.net/api/v2/menu/side-menu/detail?role_id=2
+Referrer Policy
+strict-origin-when-cross-origin
 
+2. 응답 헤더
+
+3. 요청 헤더
 accept
 application/json, text/plain, */*
 kubeflow-userid
 user.id
 namespace
-test-namespace
+ais-smoke-testing
 project-id
 85
 referer
@@ -15,6 +23,37 @@ Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)
 
 
 # A 개발 환경
+1. 제네럴
+Request URL
+https://portal.aiserving.dev.aip.domain.net/api/v2/menu/side-menu/detail?role_id=2
+Request Method
+GET
+Status Code
+200 OK
+Remote Address
+#PRIVATE_ADDRESS#
+Referrer Policy
+strict-origin-when-cross-origin
+
+2. 응답 헤더
+access-control-allow-origin
+*
+content-length
+3349
+content-type
+application/json
+date
+Mon, 18 Aug 2025 05:53:44 GMT
+server
+istio-envoy
+set-cookie
+authservice_session=MTc1NTQ5NjQyMnxOd3dBTkRKRVFsZENTazlQV2xGWlVFUklWRWRIVTBWTFZ6SlBTakpGUjB4R1JFdE5OMVJNVGpaUE1saExVRTlTTWxCWFUwSlFVMEU9fDXm8wbBrYOrJ8FElezlHCoYTVuS6nRrpHkr6_8l6d2R; Domain=.aiserving.dev.aip.domain.net; Expires=Tue, 19 Aug 2025 05:53:44 GMT; HttpOnly; Path=/
+x-envoy-upstream-service-time
+245
+
+
+
+3. 요청 헤더
 accept
 application/json, text/plain, */*
 accept-encoding
@@ -24,17 +63,17 @@ ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7
 connection
 keep-alive
 cookie
-authservice_session=MTc1NTQ4OTM4N3xOd3dBTkZkTFJraENSMGROVXpKU1ZVZElSamMzTjBOTk4wNDNXVWxUU1ZORE4xZ3lRa1EzVlVOSldFTklUVE5OVTBOSlJEUkdVa0U9fJw3jfB2fiRj-oi3ox07rKpBuu-G-B0DJtxTH3OzEBxR
+authservice_session=MTc1NTQ5NjQyMnxOd3dBTkRKRVFsZENTazlQV2xGWlVFUklWRWRIVTBWTFZ6SlBTakpGUjB4R1JFdE5OMVJNVGpaUE1saExVRTlTTWxCWFUwSlFVMEU9fDXm8wbBrYOrJ8FElezlHCoYTVuS6nRrpHkr6_8l6d2R
 host
 portal.aiserving.dev.aip.domain.net
 kubeflow-userid
 user.id
 namespace
-test-namespace
+ais-smoke-testing
 project-id
 85
 referer
-https://portal.aiserving.dev.aip.domain.net/serving/model-server?ns=test-namespace&uId=2&projId=85&isAdmin=&projName=smoke-testing&accName=user.id&roleId=2&isApiCallAllowed=true
+https://portal.aiserving.dev.aip.domain.net/dashboard
 sec-ch-ua
 "Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"
 sec-ch-ua-mobile
@@ -49,3 +88,4 @@ sec-fetch-site
 same-origin
 user-agent
 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36
+
