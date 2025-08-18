f20fe4474500e34f686ec676570b23f33d4ae4ef
daktang <90pizza@naver.com>
2025-08-18 11:59:06 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,71 +1,23 @@
+--- authenticate
+
 Request URL
-https://localhost:3000/api/v2/api/v3/authenticate
-Request Method
-POST
-Status Code
-500 Internal Server Error
-Remote Address
-127.0.0.1:3000
+http://portal.aiserving.dev.aip.domain.net/api/v3/authenticate
 Referrer Policy
 strict-origin-when-cross-origin
-access-control-allow-origin
-https://localhost:3000
-connection
-close
-content-length
-116
-content-type
-text/html; charset=utf-8
-date
-Thu, 14 Aug 2025 08:37:05 GMT
-server
-istio-envoy
-set-cookie
-authservice_session=MTc1NTE1NzM0NXxOd3dBTkVOT1QwZEdOVWhZVGtwUVZVOVBSVVpOUlZGWVNrNVFXRFZGVGxSWFVrbGFOMU5VV2xGUlIwbFBORlZJVUVsRFYxcFpXbEU9fArVCNH335IDuJ3kQW1PSMNOBBnc2yH1M2OzRnBmuTzM; Domain=.aiserving.dev.aip.domain.net; Expires=Fri, 15 Aug 2025 08:37:05 GMT; HttpOnly; Path=/
-vary
-Origin, Accept-Encoding
-x-envoy-upstream-service-time
-3
-x-powered-by
-Express
+
+request header
 accept
 application/json, text/plain, */*
-accept-encoding
-gzip, deflate, br, zstd
-accept-language
-ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7
-connection
-keep-alive
-content-length
-2
 content-type
 application/json
-cookie
-authservice_session=MTc1NTE1NzM0NXxOd3dBTkVOT1QwZEdOVWhZVGtwUVZVOVBSVVpOUlZGWVNrNVFXRFZGVGxSWFVrbGFOMU5VV2xGUlIwbFBORlZJVUVsRFYxcFpXbEU9fArVCNH335IDuJ3kQW1PSMNOBBnc2yH1M2OzRnBmuTzM
-host
-localhost:3000
 kubeflow-userid
 null
 namespace
 undefined
-origin
-https://localhost:3000
 project-id
 undefined
 referer
-https://localhost:3000/login
-sec-ch-ua
-"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"
-sec-ch-ua-mobile
-?0
-sec-ch-ua-platform
-"Windows"
-sec-fetch-dest
-empty
-sec-fetch-mode
-cors
-sec-fetch-site
-same-origin
+http://localhost:3000/
 user-agent
 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36
 x-mlops-action-id
@@ -74,3 +26,25 @@ x-mlops-menuid
 26
 x-mlops-namespace
 x-mlops-target
+
+
+--- logout
+Request URL
+https://auth.dev.aip.domain.net/auth/realms/aiplatform/protocol/openid-connect/logout?client_id=serve&post_logout_redirect_uri=http://localhost:3000/after-logout/logout?service=http://localhost/after-logout/logout
+Referrer Policy
+strict-origin-when-cross-origin
+
+
+request header
+referer
+http://localhost:3000/
+sec-ch-ua
+"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"
+sec-ch-ua-mobile
+?0
+sec-ch-ua-platform
+"Windows"
+upgrade-insecure-requests
+1
+user-agent
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36
