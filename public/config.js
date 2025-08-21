4433a9902ffdf792c693951ed80b29c3f59556bf
daktang <90pizza@naver.com>
2025-08-21 11:22:55 +0900

    Updated config.js

--- public/config.js
+++ public/config.js
@@ -24,7 +24,7 @@ window.config = {
   defaultAffinityConfig: 'servingPool',
 
   kubeflowURL: '/kubeflowproxy/',
-  kubeflowBaseUrl: 'localhost:3000/kubeflowproxy',
+  kubeflowBaseUrl: 'http://localhost:3000/kubeflowproxy', // ← 프로토콜 추가
 
   servingExtBaseUrl: '/serving',
 
