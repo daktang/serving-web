ae8d473c2ac86744b709f6842ca6278716a89f86
daktang <90pizza@naver.com>
2025-08-25 10:13:54 +0900

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-document.getElementById('root')?.removeAttribute('aria-hidden');
+(() => { const el = document.elementFromPoint(innerWidth/2, innerHeight/2); console.log('CENTER_EL:', el); console.log('STYLE:', getComputedStyle(el)); })();
