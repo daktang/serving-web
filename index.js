9289dcec3e21e36478eed90958a1d5c8d6bb18da
daktang <90pizza@naver.com>
2025-08-25 10:28:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,14 @@
-[probe] start
-VM26719:29 [probe] root init children: 2
-VM26719:35 [probe] initial URL: https://portal.serve.aip.domain.net/api-key-mgmt
-undefined
-api-key-mgmt:1 Blocked aria-hidden on an element because its descendant retained focus. The focus must not be hidden from assistive technology users. Avoid using aria-hidden on a focused element or its ancestor. Consider using the inert attribute instead, which will also prevent focus. For more details, see the aria-hidden section of the WAI-ARIA specification at https://w3c.github.io/aria/#aria-hidden.
-Element with focus: <button.MuiButtonBase-root MuiButton-root MuiButton-text btn-outline-style>
-Ancestor with aria-hidden: <div.grip-resizable#root> <div id=​"root" class=​"grip-resizable" aria-hidden=​"true">​…​</div>​Understand this warning
-APIKeyManagementList.js:135 i18next::translator: missingKey en translations New API Key New API Key
-TableHeader.js:215 i18next::translator: missingKey en translations Valid to Valid to
-TableHeader.js:227 i18next::translator: missingKey en translations Model Server Model Server
-TableHeader.js:215 i18next::translator: missingKey en translations Created at Created at
-TableHeader.js:227 i18next::translator: missingKey en translations  
-APIKeyManagementList.js:135 i18next::translator: missingKey en translations New API Key New API Key
-TableHeader.js:215 i18next::translator: missingKey en translations Valid to Valid to
-TableHeader.js:227 i18next::translator: missingKey en translations Model Server Model Server
-TableHeader.js:215 i18next::translator: missingKey en translations Created at Created at
-TableHeader.js:227 i18next::translator: missingKey en translations  
-index.js:514 i18next::translator: missingKey en translations New API Key New API Key
-index.js:165 i18next::translator: missingKey en translations CREATE CREATE
-APIKeyManagementList.js:135 i18next::translator: missingKey en translations New API Key New API Key
-TableHeader.js:215 i18next::translator: missingKey en translations Valid to Valid to
-TableHeader.js:227 i18next::translator: missingKey en translations Model Server Model Server
-TableHeader.js:215 i18next::translator: missingKey en translations Created at Created at
-TableHeader.js:227 i18next::translator: missingKey en translations  
-index.js:514 i18next::translator: missingKey en translations New API Key New API Key
-index.js:165 i18next::translator: missingKey en translations CREATE CREATE
-TableHeader.js:215 i18next::translator: missingKey en translations Valid to Valid to
-TableHeader.js:227 i18next::translator: missingKey en translations Model Server Model Server
-TableHeader.js:215 i18next::translator: missingKey en translations Created at Created at
-TableHeader.js:227 i18next::translator: missingKey en translations  
-VM26719:27 [mut] {hasChildren: true, html: '(omitted)'}
-APIKeyManagementList.js:135 i18next::translator: missingKey en translations New API Key New API Key
-TableHeader.js:215 i18next::translator: missingKey en translations Valid to Valid to
-TableHeader.js:227 i18next::translator: missingKey en translations Model Server Model Server
-TableHeader.js:215 i18next::translator: missingKey en translations Created at Created at
-TableHeader.js:227 i18next::translator: missingKey en translations  
-index.js:514 i18next::translator: missingKey en translations New API Key New API Key
-index.js:165 i18next::translator: missingKey en translations CREATE CREATE
-VM26719:27 [mut] {hasChildren: true, html: '(omitted)'}
-TableHeader.js:215 i18next::translator: missingKey en translations Valid to Valid to
-TableHeader.js:227 i18next::translator: missingKey en translations Model Server Model Server
-TableHeader.js:215 i18next::translator: missingKey en translations Created at Created at
-TableHeader.js:227 i18next::translator: missingKey en translations  
-index.js:514 i18next::translator: missingKey en translations New API Key New API Key
-index.js:165 i18next::translator: missingKey en translations CREATE CREATE
-index.js:514 i18next::translator: missingKey en translations New API Key New API Key
-index.js:165 i18next::translator: missingKey en translations CREATE CREATE
-VM26719:19 [window.error] Uncaught TypeError: Cannot read properties of undefined (reading 'filter') TypeError: Cannot read properties of undefined (reading 'filter')
-    at useAutocomplete (useAutocomplete.js:225:1)
-    at Autocomplete (Autocomplete.js:386:1)
-    at Ch (react-dom.production.min.js:157:1)
-    at gi (react-dom.production.min.js:176:1)
-    at ck (react-dom.production.min.js:271:1)
-    at bk (react-dom.production.min.js:250:1)
-    at ak (react-dom.production.min.js:250:1)
-    at Tj (react-dom.production.min.js:250:1)
-    at Lj (react-dom.production.min.js:243:1)
-    at eval (react-dom.production.min.js:123:1)
-VM26719:27 [mut] {hasChildren: false, html: '(empty)'}
+var focusTag = useEventCallback(function (tagToFocus) {
+    if (tagToFocus === -1) {
+      inputRef.current.focus();
+    } else {
+      anchorEl.querySelector("[data-tag-index=\"".concat(tagToFocus, "\"]")).focus();
+    }
+  }); // Ensure the focusedTag is never inconsistent
+
+  React.useEffect(function () {
+    if (multiple && focusedTag > value.length - 1) {
+      setFocusedTag(-1);
+      focusTag(-1);
+    }
+  }, [value, multiple, focusedTag, focusTag]);
