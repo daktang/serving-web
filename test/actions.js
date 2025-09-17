0f93ceb63a197e31af9fd199cdc3be2a478447f8
daktang <90pizza@naver.com>
2025-09-17 14:36:28 +0900

    Update actions.js

--- test/actions.js
+++ test/actions.js
@@ -1,13 +1,20 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/style/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css">
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAq06l5RUVfib62IYRQacLc-KAy0XIWAVs"></script>
-  </head>
-  <body>
-    <div class="container"></div>
-  </body>
-  <script src="/bundle.js"></script>
-</html>
+import axios from 'axios';
+
+const API_KEY = 'a31aba5005de931f98a94ada77b3c10a';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;  //ES6 Template Option
+
+// 1) Action type을 상수로 정의하는 이유는, 다른 곳에서 사용할 때 오타로 인한 버그를 줄이기 위함.
+// 2) Reducer과 Action등 어디서든 불러와서 사용할 때 Key값으로 사용하기 위함.
+// 3) String이 변하게 되어도 Action / Reducer에서 변경점이 없음.
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url); //request promise
+  
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
+
